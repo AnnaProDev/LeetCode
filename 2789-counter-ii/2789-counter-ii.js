c9a14d2199eb8ac3628f51b0aabe3c8e151bd869
Anna Kondrateva <85047120+AnnaProDev@users.noreply.github.com>
2024-06-21 15:52:31 -0400

    Time: 53 ms (85.63%), Space: 51.2 MB (84.57%) - LeetHub

---
+++ 2789-counter-ii/2789-counter-ii.js
@@ -0,0 +1,30 @@
+/**
+ * @param {integer} init
+ * @return { increment: Function, decrement: Function, reset: Function }
+ */
+var createCounter = function(init) {
+    let p = init;
+    return {
+        increment: function() {
+            p = p + 1;
+            return ( p )
+            init = p
+        },
+        decrement: function() {
+            p = p - 1
+            return ( p )
+            init = p
+        },
+        reset: function() {
+            p = init
+            return ( p )
+        }
+     }
+};
+
+/**
+ * const counter = createCounter(5)
+ * counter.increment(); // 6
+ * counter.reset(); // 5
+ * counter.decrement(); // 4
+ */
